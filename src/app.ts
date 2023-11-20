848fb38a9d0ab2aae9c6e35e9b8ef768449a27ad
Rohan Kaushik <rohankaushik1@outlook.com>
2023-11-20 15:49:19 +0530

    rendering ejs partials / home page

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from 'url';
 
 const app = express()
 const port = 3000
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, '../public')))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, '../views'));
+
+app.get("/",(req,res) => {
+    res.render('home')
+})
+
 app.listen(port,() => {
     console.log(`server running on port ${port}`)
 })
+
